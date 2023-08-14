b00fc217870e228984a8f3fc5d6d6217000a37a8
benjask8 <rosanojuan65@gmail.com>
2023-08-13 21:33:58 -0300

    Footer agregado, bugs visualesde pagina de login arreglados

--- src/views/home/hero.php
+++ src/views/home/hero.php
@@ -1,6 +1,6 @@
 <section class="hero">
     <section class="hero-form">Hero Formulario</section>
-    <section class="hero-txt">Viaja con Via<span style="font-size:1em;color:rgb(51, 87, 153);">Uy</span> viaja, <span id="hero-txt-span" class="hero-txt-span">Facil</span></section>
+    <section class="hero-txt">Viaja con Via<span style="font-size:1em;color:rgb(51, 87, 153);">Uy</span>, viaja <span id="hero-txt-span" class="hero-txt-span">Facil</span></section>
 </section>
 
 <script src="/via_uy/src/public/js/home/hero.js"></script>
--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -1,8 +1,38 @@
     </section>
 
 
-    <h1>Footer</h1>
-    
+    <footer class="footer">
+  <section class="footer-links-container">
+    <nav class="footer-links">
+      <p class="footer-links-title">Servicios</p>
+      <a href="/registro-empresas" class="footer-links-a">Registro de Empresas de Ómnibus</a>
+      <a href="/gestion-rutas" class="footer-links-a">Gestión de Rutas</a>
+      <a href="/flota-buses" class="footer-links-a">Flota de Buses</a>
+    </nav>
+    <nav class="footer-links">
+      <p class="footer-links-title">Recursos</p>
+      <a href="/blog" class="footer-links-a">Blog</a>
+      <a href="/tutoriales" class="footer-links-a">Tutoriales</a>
+      <a href="/contacto" class="footer-links-a">Contacto</a>
+    </nav>
+    <nav class="footer-links">
+      <p class="footer-links-title">Acerca de ViaUY</p>
+      <a href="/acerca" class="footer-links-a">Sobre Nosotros</a>
+      <a href="/equipo" class="footer-links-a">Nuestro Equipo</a>
+      <a href="/testimonios" class="footer-links-a">Testimonios</a>
+    </nav>
+    <nav class="footer-links">
+      <p class="footer-links-title">Redes Sociales</p>
+      <a href="https://www.facebook.com/ViaUY" target="_blank" class="footer-links-a">Facebook</a>
+      <a href="https://twitter.com/ViaUY" target="_blank" class="footer-links-a">Twitter</a>
+      <a href="https://www.linkedin.com/company/viauy" target="_blank" class="footer-links-a">LinkedIn</a>
+    </nav>
+  </section>
+  <section class="footer-cr">© 2023 ViaUY - Plataforma de Gestión para Empresas de Ómnibus. Todos los derechos reservados.</section>
+</footer>
+
+
+
 
     <script src="/via_uy/src/public/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
